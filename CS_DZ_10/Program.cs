28e385f0b045500542e2d82184932cfaa31bd895
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-09 10:52:24 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frameWidht = "";
+            string name;
+            string symbol;
+            string emptySymbol = " ";
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите символ: ");
+            symbol = Console.ReadLine();
+
+            for (int i = 1; i <= (name.Length + 4); i += 1)
+            {
+                frameWidht += symbol;
+            }
+
+            Console.WriteLine(frameWidht);
+            Console.WriteLine(frameWidht [0] + emptySymbol + name + emptySymbol + frameWidht [frameWidht.Length - 1]);
+            Console.WriteLine(frameWidht);
+        }
+    }
+}
